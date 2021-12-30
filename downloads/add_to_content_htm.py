7d0cd6556136b463b1d9ff474f0fdcadb23e9f23
mdecourse <mdecourse@gmail.com>
2021-12-30 16:52:27 +0800

    update add to content.htm program

--- downloads/add_to_content_htm.py
+++ downloads/add_to_content_htm.py
@@ -3,25 +3,35 @@ from bs4 import BeautifulSoup
 # ref: https://stackoverflow.com/questions/15233340/getting-rid-of-n-when-using-readlines
 # ref: https://stackoverflow.com/questions/3845423/remove-empty-strings-from-a-list-of-strings
 
-with open("1a_grouping.txt", encoding="utf-8") as f:
-    data = f.read().splitlines()
-data = list(filter(None, data))
-#print(data)
-for i in data:
-    print(i)
-
 '''
 設法讀出各班各組學員學號資料
 '''
 repo_path = "Y:/tmp/cp2021_final"
-class_name = "1b"
-group_file = repo_path + "/" + class_name + "_group.txt"
-student_id = "41023208"
+class_name = "1a"
+group_file = repo_path + "/downloads/" + class_name + "_grouping.txt"
+
+with open(group_file, encoding="utf-8") as f:
+    data = f.read().splitlines()
+data = list(filter(None, data))
+#print(data)
+grp_count = 0
+
+for i in data:
+    if class_name in i:
+        mem_count = 0
+        grp_count += 1
+        grp = i
+        print("組別:", grp_count, grp)
+    else:
+        mem_count += 1
+        student_id = i
+        print("學員:", mem_count, student_id)
+
 
 '''
 根據學號, 從 downloads 目錄取出該學員的期末報告網頁 
 '''
-
+'''
 html_location = repo_path + "/downloads/" + student_id + "_html.txt"
 with open(html_location, encoding="utf-8") as f:
     student_html = f.read()
@@ -53,6 +63,6 @@ output = str(soup)
 output = output.replace("<html><body", "")
 output = output.replace("</body></html>", "")
 print(output)
-
+'''
 
 
