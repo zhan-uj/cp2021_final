a52c2446e6d19e222380aa3c2ad026583a6a3c75
mdecourse <mdecourse@gmail.com>
2021-12-30 14:43:19 +0800

    add basic add to content.htm program

--- downloads/add_to_content_htm.py
+++ downloads/add_to_content_htm.py
@@ -1,12 +1,58 @@
+from bs4 import BeautifulSoup
+
 # ref: https://stackoverflow.com/questions/15233340/getting-rid-of-n-when-using-readlines
 # ref: https://stackoverflow.com/questions/3845423/remove-empty-strings-from-a-list-of-strings
+
 with open("1a_grouping.txt", encoding="utf-8") as f:
     data = f.read().splitlines()
 data = list(filter(None, data))
 #print(data)
 for i in data:
     print(i)
-'''
-['1a grouping', '1ag1', '41023146', '41023143', '41023147', '41023113', '41023116', '41023111', '41023145', '1ag2', '41023104', '41023103', '41023105', '41023106', '41023107', '41023109', '1ag3', '41023125', '41023119', '41023120', '41023124', '41023118', '41023122', '41023130', '1ag4', '40923129', '41023135', '41023150', '41023151', '41023152', '40923115', '41023132', 'lag5', '41023114', '41023126', '41023101', '41023110', '41023108', '40823214', 'lag6', '41023121', '41023140', '41023133', '41023112', '41023153', '41023134', '41023138', '40832244', 'lag7', '41023142', '41023137', '41023129', '41023131', '41023127', '41023141', '40823227', '41023154']
 
 '''
+設法讀出各班各組學員學號資料
+'''
+repo_path = "Y:/tmp/cp2021_final"
+class_name = "1b"
+group_file = repo_path + "/" + class_name + "_group.txt"
+student_id = "41023208"
+
+'''
+根據學號, 從 downloads 目錄取出該學員的期末報告網頁 
+'''
+
+html_location = repo_path + "/downloads/" + student_id + "_html.txt"
+with open(html_location, encoding="utf-8") as f:
+    student_html = f.read()
+
+#print(student_html)
+
+# 動態網頁檔案所在路徑
+file_location = repo_path + "/config/content.htm"
+# 將動態網頁檔案內容讀出, 存入 data 變數區
+with open(file_location, encoding="utf-8") as f:
+    data = f.read()
+
+#print(data)
+
+# 利用 data 建立 soup
+soup = BeautifulSoup(data, "lxml")
+# 利用 student_html 建立 soup
+soup2 = BeautifulSoup(student_html, 'lxml')
+# 插入的 student_html 會多出 <html><body></body></html>
+# 最後存檔前必須移除
+for i in soup.find_all('h2'):
+    if i.text == "1ag1":
+        # 取 soup2 中的 tag
+        for j in soup2:
+        # 在 soup 中對應 <h2>1ag1</h2> tag 之後插入 j tag
+            i.insert_after(j)
+
+output = str(soup)
+output = output.replace("<html><body", "")
+output = output.replace("</body></html>", "")
+print(output)
+
+
+
